4b8043354c1292ab800c3aafec8229b8f42a3be3
Longlong Li <lilong7676@outlook.com>
2023-03-08 17:25:56 +0800

    fix: 修复 monaco-editor、devtools 加载失败

--- packages/website/components/devtools/index.tsx
+++ packages/website/components/devtools/index.tsx
@@ -3,7 +3,7 @@
  * @Author: lilonglong
  * @Date: 2023-01-16 23:09:20
  * @Last Modified by: lilonglong
- * @Last Modified time: 2023-03-08 11:05:09
+ * @Last Modified time: 2023-03-08 17:10:43
  */
 
 import { GatewayPort } from '@mini_faas_worker/common';
@@ -21,11 +21,11 @@ export default function Devtools(props: IProps) {
 
   const host = IS_DEV
     ? `http://localhost:${GatewayPort}`
-    : `window.location.origin/mini_faas_worker/gateway`;
+    : `${window.location.origin}/mini_faas_worker/gateway`;
 
   const wsUrl = IS_DEV
     ? `ws=localhost:${GatewayPort}`
-    : 'wss=lilong7676.cn/mini_faas_worker/gateway/ws';
+    : `wss=${window.location.host}/mini_faas_worker/gateway/ws`;
 
   const devtoolsPath = `/front_end/devtools_app.html?v8only=true&${wsUrl}/debugger/${debuggerSessionId}`;
 
--- packages/website/components/monaco-editor/index.tsx
+++ packages/website/components/monaco-editor/index.tsx
@@ -3,14 +3,19 @@
  * @Author: lilonglong
  * @Date: 2023-01-12 23:19:58
  * @Last Modified by: lilonglong
- * @Last Modified time: 2023-01-12 15:25:59
+ * @Last Modified time: 2023-03-08 17:24:18
  */
 import Editor from '@monaco-editor/react';
 import { loader } from '@monaco-editor/react';
+
+const IS_DEV = process.env.NODE_ENV === 'development';
+
 const loaderConfig = {
   paths: {
     // 临时解决默认使用 jsdeliver cdn 访问缓慢的问题
-    vs: '/monaco-editor/min/vs',
+    vs: IS_DEV
+      ? '/monaco-editor/min/vs'
+      : 'https://lilong7676.cn/mini_faas_worker/monaco-editor/min/vs',
   },
 };
 loader.config(loaderConfig);
