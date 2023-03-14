247f8ba0beac54df5714e72181318ef3e9f102b1
Longlong Li <lilong7676@outlook.com>
2023-03-14 15:24:52 +0800

    fix: run.mini_faas_worker.sh support docker volumes

--- scripts/run.mini_faas_worker.sh
+++ scripts/run.mini_faas_worker.sh
@@ -12,5 +12,5 @@ docker pull lilong7676/mini_faas_worker:latest
 echo "pull latest done"
 
 echo "run mini_faas_worker"
-docker run --name="mini_faas_worker" -d -p 9100:9100 -p 9101:9101 -p 9102:9102 lilong7676/mini_faas_worker:latest
+docker run --name="mini_faas_worker" -v /root/fakeOSS:/usr/local/mini_faas_worker/packages/gateway/.fakeOSS -d -p 9100:9100 -p 9101:9101 -p 9102:9102 lilong7676/mini_faas_worker:latest
 echo "run mini_faas_worker done"
